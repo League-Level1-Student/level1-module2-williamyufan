115dbea777f2547f4963397c2cb0121fa07377c4
williamyufan <37283310+williamyufan@users.noreply.github.com>
2019-12-11 20:07:04 -0800

    finished the first part

---
+++ src/_01_getters_and_setters/Taco.java
@@ -0,0 +1,21 @@
+package _01_getters_and_setters;
+
+public class Taco {
+	private String meat;
+	private String sauce;
+public static void main(String[] args) {
+	
+}
+ String getMeat(){
+	return meat;
+}
+ String getSauce(){
+	 return sauce;
+ }
+ void setMeat(String meat){
+	 
+ }
+ void setSauce(String sauce){
+	 
+ }
+}
