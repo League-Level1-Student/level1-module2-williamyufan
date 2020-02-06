4a7745f7f29005080211aca38da65b75f6853cd7
league <league@imac-22.attlocal.net>
2020-02-05 19:56:46 -0800

    finished module 2

--- src/_07_tea_party/Tea_party.java
+++ src/_07_tea_party/Tea_party.java
@@ -1,9 +1,30 @@
 package _07_tea_party;
 
-	public class Tea_party {
+
+
+public class Tea_party {
         public String welcome(String name, boolean isWoman, boolean isKnighted) {
+        if(isKnighted & isWoman) {
+        	return "Welcome Lady" + name;
+        }
+        else if(isKnighted & isWoman==false) {
+        	
+        	return "Welcome Sir" + name;
+        	
+        }
+        else if(isKnighted==false & isWoman) {
+        return "Welcome Mrs." + name;
+        	
+        }
+        else{
+        	return "Welcome Mr." + name;
+        }
+		
+        
         
         }
 }
 
-}
+
+
+
